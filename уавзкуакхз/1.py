0a02ea6751bb5c19f9918ec3098afe1a3dd656fc
ezhik2011 <fedor.nesterov.11@mail.ru>
2023-03-24 17:45:42 +0300

    начали делать алфавит

--- уавзкуакхз/1.py
+++ уавзкуакхз/1.py
@@ -1,28 +1,38 @@
-
-
 def privetstvie():
     name = (input('Как вас зовут ? '))
     print('Привет',name,)
-pass
 
-name_2 = (input('Что будем делать:зашифровывать или расшифровывать Если зашифровывать, то выберете  шифр: Шифр Цезаря, Шифр Вижинера,  Шифр Вернама, '))
 
-if 1:
+name_2 = (input('Что будем делать: зашифровывать или расшифровывать. Если зашифровывать, то выберете  шифр: Шифр Цезаря, Шифр Вижинера,  Шифр Вернама, '))
 
-        print(' Шифр Цезаря ')
+print(name_2)
+
+print(' Шифр Цезаря ')
         
-        print(name_2)
+letters =  ['а', 'б']
+
+def chifr_cezary():
+    name_4 = int(input('Введите желаемый сдвиг числом от 1 до 33 на сколько хотите сместить  '))
+    message = input("Введите сообщение ")
+    for letter in message:
+        
+
+
+
+
+
 
 
 def encode_shifr_caesar():
     pass
-encode_shifr_caesar = int(input('Введите текст который хотите зашифровать '))
-def decode_shifr_caesar():
+def  encode_shifr_caesar = int(input('Введите текст который хотите зашифровать '))
+
     pass
 decode_shifr_caesar = int(input('Введите шифр который хотите расшифровать '))
 
 name_4 = int(input('Введите чило от на сколько хотите сместить 1 до 33 '))
 print(name_4)
+
 
 
 
