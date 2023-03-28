4c8a76433f25d28b480576a2147a267e083a56f5
ezhik2011 <fedor.nesterov.11@mail.ru>
2023-03-28 18:51:45 +0300

    начали делать цикл

--- уавзкуакхз/1.py
+++ уавзкуакхз/1.py
@@ -1,37 +1,33 @@
 def privetstvie():
     name = (input('Как вас зовут ? '))
     print('Привет',name,)
+pass
+def  encode_shifr_caesar():
 
 
+
+
+    letters =  ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','э','ю','я']
+name_3 = int(input(''))
+def chifr_cezary():
+    name_4 = int(input('Введите желаемый сдвиг числом от 1 до 30 на сколько хотите сместить  '))
+    message = input(' Введите текст который хотите зашифровать')
+    for letter in message:
+        pass
 name_2 = (input('Что будем делать: зашифровывать или расшифровывать. Если зашифровывать, то выберете  шифр: Шифр Цезаря, Шифр Вижинера,  Шифр Вернама, '))
 
 print(name_2)
-
 print(' Шифр Цезаря ')
-        
-letters =  ['а', 'б']
 
-def chifr_cezary():
-    name_4 = int(input('Введите желаемый сдвиг числом от 1 до 33 на сколько хотите сместить  '))
-    message = input("Введите сообщение ")
-    for letter in message:
-        
 
+#def  encode_shifr_caesar():
+#encode_shifr = int(input('Введите текст который хотите зашифровать '))
+
+#pass
+#decode_shifr_caesar = int(input('Введите шифр который хотите расшифровать '))
 
 
 
-
-
-
-def encode_shifr_caesar():
-    pass
-def  encode_shifr_caesar = int(input('Введите текст который хотите зашифровать '))
-
-    pass
-decode_shifr_caesar = int(input('Введите шифр который хотите расшифровать '))
-
-name_4 = int(input('Введите чило от на сколько хотите сместить 1 до 33 '))
-print(name_4)
 
 
 
